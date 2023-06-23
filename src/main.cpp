ab224e2b5f24dd393a51195748ec6d0abe2bdf4c
Luke Fisher <lukedavidfisher03@gmail.com>
2023-06-23 12:51:56 -0400

    created access point; started to serve html files from AP

--- src/main.cpp
+++ src/main.cpp
@@ -1,97 +1,91 @@
 #include <Arduino.h>
 #include <WiFi.h>
+#include <SPIFFS.h>
 
-char ssid[] = "SSID";
-char passwd[] = "PASSWORD";
+#define FORMAT_SPIFFS_IF_FAILED true
+#define OPEN_NETWORK true
+
+#if OPEN_NETWORK == true
+    const char* passwd = NULL;
+#else
+    char passwd[] = "password";
+#endif
+
+
+
+char ssid[] = "ALARM_BUDDY";
 int WIFI_TIMEOUT_LENGTH = 10;
+File webpage;
+
+
 WiFiServer server(80);
 
-bool wifiConnect(char [], char []);
+void setup() { 
+    Serial.begin(9600);
+    
+    WiFi.softAP(ssid,passwd,1, 0, 1);   // Create soft access point that only allows one users at a time.
+    IPAddress server_ip = WiFi.softAPIP();
+    Serial.println("WiFi connection created, serving at: ");
+    Serial.print(server_ip);
 
-void setup() {
-  // put your setup code here, to run once:
-  Serial.begin(9600);
-  bool wifi_status = wifiConnect(ssid, passwd);
-
-  if(wifi_status) {
-    Serial.println("Starting web server...");
     server.begin();
-    IPAddress ip = WiFi.localIP();
-    Serial.println("Web server started at: ");
-    Serial.print(ip);
-  } else {
-    Serial.println("Web server not started, not connected to the internet!");
-  }
 
-  Serial.println();
+    if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED)) {
+        Serial.println("SPIFFS filesystem failed to mount!");
+        return;
+    } else {
+        Serial.println("SPIFFS filesystem successfully mounted!");
+    }
+
+    webpage = SPIFFS.open("/index.html");
+    if (!webpage) {
+        Serial.println("ERROR OPENING FILE!");
+        return;
+    }
+
+    Serial.println("FILE CONTENTS:");
+    while(webpage.available()) {
+        Serial.write(webpage.read());
+    }
+
+    webpage.close();
 }
 
 void loop() {
-  WiFiClient client = server.available();
-  if(client) {
-    Serial.println("CLIENT CONNECTED!");
+    WiFiClient client = server.available();
 
-    bool currentLineIsBlank = true;
+    if (client) {
+        Serial.println("New client just connected....");
+        String client_data = "";
+        
+        while (client.connected()) {
+            if (client.available()) {
+                char c = client.read();
 
-    while(client.connected()) {
-      if(client.available()) {
-        char c = client.read();
-        Serial.write(c);
+                Serial.write(c);
 
-        if (c == '\n' && currentLineIsBlank) {
-          client.println("HTTP/1.1 200 OK");
-          client.println("Content-Type: text/html");
-          client.println("Connection: close");  // the connection will be closed after completion of the response
-          client.println("Refresh: 5");  // refresh the page automatically every 5 sec
-          client.println();
-          client.println("<!DOCTYPE HTML>");
-          client.println("<html>");
-          client.println("<h1>Welcome to the esp32 webpage</h1>");
-          client.println("</html>");
+                if (c == '\n') {
+                    if (client_data.length() == 0) {
+                        client.println("HTTP/1.1 200 OK");
+                        client.println("Content-type:text/html");
+                        client.println();
 
+                        client.write(webpage.read());
+                        client.println();
+                        break;
+                    } else {
+                        client_data = "";
+                    }
+                } else if (c != '\r') {
+                    client_data += c;
+                }
+            }
         }
 
-        if (c == '\n') {
-          currentLineIsBlank = true;
-        } else if (c != '\r') {
-          currentLineIsBlank = false;
-        }        
-
-      }
+        client.stop();
+        Serial.println("Client disconnected");
     }
 
-    delay(1);
-    client.stop();
-    Serial.println("Client connection completed!");
-  }
+
+
 }
-
-bool wifiConnect(char ssid[], char passwd[]) {
-  /***
-   * Connect to a wifi network
-   * 
-   * Params: ssid, passwd
-   * Returns: Boolean indicating connection status
-  */
-  bool connected = false;
-  Serial.println("Attempting to connect to network: ");
-  Serial.print(ssid);
-
-  WiFi.begin(ssid, passwd);
-  for(int i = 0; i < WIFI_TIMEOUT_LENGTH; i++) {
-    delay(1000);
-    Serial.println("Connection in progress...");
-    Serial.print(i);
-    Serial.print(" seconds passed");
-    Serial.println();
-    if(WiFi.status() == WL_CONNECTED) {
-      connected = true;
-      break;
-    }
-  }
-
-  (connected == true) ? Serial.println("Connection successful!") : Serial.println("Connection failed!");
-
-  return connected;
-}
-
