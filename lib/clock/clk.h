6d676388af650b636550098580e1246df435b099
weaverpu <paul.weaver7799@gmail.com>
2023-06-28 20:34:30 -0400

    Added accessor methods and return values to some mutator functions if appropriate. Removed namespaces, updated notation. Will separate files later.

--- lib/clock/clk.h
+++ lib/clock/clk.h
@@ -5,53 +5,51 @@
 #include <chrono>
 #include <thread>
 
-using namespace std;
-using namespace std::this_thread;
-using namespace std::chrono;
-
-
-class clk{
+class Clk{
     private:
-        int hours, minutes, second, increment_flag, selection_int;
-        string time, meridiem;
-        int* modes [3] = { &hours, &minutes, &second};
+        int hours, minutes, seconds, increment_flag, selection_int, meridiem_set;
+        std::string time, meridiem;
+        int* modes [3] = { &hours, &minutes, &seconds};
         int* selector;
     public:
         // simple constructor
         clk(){
-            hours = 12;
+            hours = 11;
             minutes = 59;
-            second = 0;
+            seconds = 30;
             selection_int = 0;
             meridiem = "A.M.";
+            meridiem_set = 0;
             increment_flag = 1;
             selector = modes[selection_int];
         };
         
         // main function to be run during loop() in main.cpp
         void tick(){
-            sleep_for(seconds(1));
-            second++;
+            std::this_thread::sleep_for(std::chrono::seconds(1));
+            seconds++;
             shift();
             update_time();
         };
 
         // mutator function used to keep each time segment in their respective ranges
         void shift(){
-            if(second == 60){
-                second = 0;
+            if(seconds == 60){
+                seconds = 0;
                 minutes++;
             }
             if(minutes == 60){
                 minutes = 0;
                 hours++;
             }
-            if(hours == 12){
-                if(meridiem.compare("P.M") == 0){
-                    meridiem = "A.M.";
+            if(hours == 12 && minutes == 0 && seconds == 0){
+                if(meridiem_set == 0){
+                    meridiem = "P.M";
+                    meridiem_set = 1;
                 }
                 else{ 
-                    meridiem = "P.M.";
+                    meridiem = "A.M.";
+                    meridiem_set = 0;
                 }
             }
             if(hours == 13){
@@ -59,17 +57,17 @@ class clk{
             }
         };
 
-        // mutator function that updates the time string
+        // This mutator function updates the time string
         void update_time(){
-            string h_string = to_string(hours);
-            string m_string = to_string(minutes);
-            string s_string = to_string(second);
+            std::string h_string = std::to_string(hours);
+            std::string m_string = std::to_string(minutes);
+            std::string s_string = std::to_string(seconds);
             time = h_string + ":" + m_string + ":" + s_string + " " + meridiem;
         }
 
         // This mutator function will be triggered by a button push, change which time segment is
-        // being incremented (hours, minutes, or seconds)
-        void change_selector(){
+        // being incremented (hours, minutes, or seconds), returns the address the selector is pointing to
+        int* change_selector(){
             if (selection_int == 2){
                 selection_int = 0;
             }
@@ -77,32 +75,41 @@ class clk{
                 selection_int++;
             }
             selector = modes[selection_int];
-            
+            return selector;
         }; 
 
         //This mutator function will be button triggered, increment selected time segment
-        void initiate_increment(){
+        // retuns the current time data segment being edited
+        int initiate_increment(){
             *selector = *selector + increment_flag;
+            return *selector;
         };
 
-        // accessor print function outputs the time (used for testing clock functionality)
-        void print_time(){
-            cout << time << "\n";
-        };
-        // accessor print function (test select mode function)
-        void print_mode(){
-            cout << *modes[selection_int] << "\n";
-        };
-
-        // This  mutator function will be button triggered and set increment type to increase or decrease
-        void set_increment(){
+        // This  mutator function will be button triggered and set increment type to increase or decrease,
+        // returns increment flag
+        int set_increment(){
             increment_flag = increment_flag * -1;
             if (increment_flag == 1){
-                cout << "Clock will increase values set";
+                std::cout << "Clock will increase values set";
             }
             else{
-                cout << "Clock will decrease values selected";
+                std::cout << "Clock will decrease values selected";
             }
+            return increment_flag;
         };
 
+        // accessor fucntion returns the current time string
+        std::string return_time(){
+            return time;
+        };
+
+        // accessor function returns the current selector address
+        int* return_selector(){
+            return selector;
+        };
+
+        // accessor function , returns which time division is being edited
+        int return_mode(){
+            return *modes[selection_int];
+        };
 };
