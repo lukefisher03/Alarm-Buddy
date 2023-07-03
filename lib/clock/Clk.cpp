a3b3d89f1139ffb95d87c04036bfe56ee9e27599
weaverpu <paul.weaver7799@gmail.com>
2023-07-03 16:50:25 -0400

    Included an overload constructor to supply time, cleaned up some code to make it easier to read

--- lib/clock/Clk.cpp
+++ lib/clock/Clk.cpp
@@ -1,5 +1,3 @@
-#include "Clk.h"
-
 #include <iostream>
 #include <string>
 #include <stdlib.h>
@@ -7,33 +5,30 @@
 #include <chrono>
 #include <thread>
 
-        // simple constructor
-Clk::Clk(){
-    hours = 11;
-    minutes = 59;
-    seconds = 30;
-    selection_int = 0;
-    meridiem = "A.M.";
-    meridiem_set = 0;
-    increment_flag = 1;
-    selector = modes[selection_int];
-};
-        
-        // main function to be run during loop() in main.cpp
+#include "Clk.h"
+
+
+Clk::Clk()
+    : hours(11), minutes(59), seconds(30), increment_flag(1), selection_int(0), meridiem_set(0), meridiem("A.M."), selector(modes[selection_int]) {}
+
+Clk::Clk(int h, int m, int s)  
+    : hours(h), minutes(m), seconds(s), increment_flag(1), selection_int(0), meridiem_set(0), meridiem("A.M."), selector(modes[selection_int]) {}
+
 void Clk::tick(){
+    // main function to be run during loop() in main.cpp
     std::this_thread::sleep_for(std::chrono::seconds(1));
     seconds++;
     shift();
     update_time();
 };
 
-        // mutator function used to keep each time segment in their respective ranges
 void Clk::shift(){
-    if(seconds == 60){
+    // mutator function used to keep each time segment in their respective ranges
+    if(seconds >= 60){
         seconds = 0;
         minutes++;
     }
-    if(minutes == 60){
+    if(minutes >= 60){
         minutes = 0;
         hours++;
     }
@@ -52,17 +47,17 @@ void Clk::shift(){
     }
 };
 
-        // This mutator function updates the time string
 void Clk::update_time(){
+    // This mutator function updates the time string
     std::string h_string = std::to_string(hours);
     std::string m_string = std::to_string(minutes);
     std::string s_string = std::to_string(seconds);
     time = h_string + ":" + m_string + ":" + s_string + " " + meridiem;
 }
 
-// This mutator function will be triggered by a button push, change which time segment is
-// being incremented (hours, minutes, or seconds), returns the address the selector is pointing to
 int* Clk::change_selector(){
+    /* This mutator function will be triggered by a button push, change which time segment is
+    being incremented (hours, minutes, or seconds), returns the address the selector is pointing to */
     if (selection_int == 2){
         selection_int = 0;
     }
@@ -73,16 +68,16 @@ int* Clk::change_selector(){
     return selector;
 }; 
 
-//This mutator function will be button triggered, increment selected time segment
-// retuns the current time data segment being edited
 int Clk::initiate_increment(){
+    /* This mutator function will be button triggered, increment selected time segment
+    retuns the current time data segment being edited */
     *selector = *selector + increment_flag;
     return *selector;
 };
 
-// This  mutator function will be button triggered and set increment type to increase or decrease,
-// returns increment flag
 int Clk::set_increment(){
+    /* This  mutator function will be button triggered and set increment type to increase or decrease,
+    returns increment flag*/
     increment_flag = increment_flag * -1;
     if (increment_flag == 1){
         std::cout << "Clock will increase values set";
@@ -93,17 +88,17 @@ int Clk::set_increment(){
     return increment_flag;
 };
 
-// accessor fucntion returns the current time string
 std::string Clk::return_time(){
+    // accessor fucntion returns the current time string
     return time;
 };
 
-// accessor function returns the current selector address
 int* Clk::return_selector(){
+    // accessor function returns the current selector address
     return selector;
 };
 
-// accesssor function , returns which time division is being edited
 int Clk::return_mode(){
+    // accesssor function , returns which time division is being edited
     return *modes[selection_int];
 };
--- lib/clock/Clk.h
+++ lib/clock/Clk.h
@@ -5,6 +5,10 @@
 #include <chrono>
 #include <thread>
 
+
+#ifndef CLK_H
+#define CLK_H
+
 class Clk{
     private:
         int hours, minutes, seconds, increment_flag, selection_int, meridiem_set;
@@ -12,35 +16,17 @@ class Clk{
         int* modes [3] = { &hours, &minutes, &seconds};
         int* selector;
     public:
-        // simple constructor
         Clk();
-        
-        // main function to be run during loop() in main.cpp
+        Clk(int h, int m, int s);
         void tick();
-        // mutator function used to keep each time segment in their respective ranges
         void shift();
-
-        // This mutator function updates the time string
         void update_time();
-
-        // This mutator function will be triggered by a button push, change which time segment is
-        // being incremented (hours, minutes, or seconds), returns the address the selector is pointing to
         int* change_selector();
-
-        //This mutator function will be button triggered, increment selected time segment
-        // retuns the current time data segment being edited
         int initiate_increment();
-
-        // This  mutator function will be button triggered and set increment type to increase or decrease,
-        // returns increment flag
         int set_increment();
-
-        // accessor fucntion returns the current time string
         std::string return_time();
-
-        // accessor function returns the current selector address
         int* return_selector();
-
-        // accessor function , returns which time division is being edited
         int return_mode();
 };
+
+#endif
